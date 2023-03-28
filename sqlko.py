c66f86c78bcc7b226b66fb85f17be7c3f1b5e6c6
Endgar <ntd1645@gmail.com>
2023-03-28 11:04:03 +0200

    new file:   prijmeni.txt 	new file:   skript.sql 	new file:   sqlko.py

---
+++ sqlko.py
@@ -0,0 +1,11 @@
+with open('prijmeni.txt', 'r') as f:
+    prijmeni = f.readlines()
+
+with open('skript.sql', 'w') as f:
+    for prijmeni in prijmeni:
+        prijmeni = prijmeni.strip()
+        f.write(f"CREATE DATABASE {prijmeni} CHARACTER SET utf8 COLLATE utf8_czech_ci;\n")
+        f.write(f"CREATE USER '{prijmeni}'@'localhost' IDENTIFIED BY '{prijmeni}123.';\n")
+        f.write(f"GRANT ALL PRIVILEGES ON {prijmeni}.* TO '{prijmeni}'@'localhost' WITH GRANT OPTION;\n")
+
+
