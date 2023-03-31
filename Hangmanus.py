2c2e0566acf1bb171763843bac174615fefdb17c
Endgar <ntd1645@gmail.com>
2023-03-31 11:05:16 +0200

    modified:   Hangmanus.py 	modified:   words.py

--- Hangmanus.py
+++ Hangmanus.py
@@ -1,28 +1,28 @@
 import random
-from words import word_list
+from words import list_slov
 
 
 def get_word():
-    word = random.choice(word_list)
-    return word.upper()
+    slovo = random.choice(list_slov)
+    return slovo.upper()
 
 
-def play(word):
-    word_completion = "_" * len(word)
+def play(slovo):
+    word_completion = "_" * len(slovo)
     guessed = False
     guessed_letters = []
     guessed_words = []
     pokus = 6
     print("Hrajeme ŠIBENICI!")
     print(display_hangman(pokus))
-    print(word_completion)match
+    print(word_completion)
     print("\n")
     while not guessed and pokus > 0:
         guess = input("Hádej slovo nebo písmenko: ").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("Už si hádal tohle písmenko!", guess)
-            elif guess not in word:
+            elif guess not in slovo:
                 print(guess, "není ve slově :(.")
                 pokus -= 1
                 guessed_letters.append(guess)
@@ -30,22 +30,22 @@ def play(word):
                 print("Dobrá práce,", guess, "je ve slově!")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(word) if letter == guess]
+                indices = [i for i, letter in enumerate(slovo) if letter == guess]
                 for index in indices:
                     word_as_list[index] = guess
                 word_completion = "".join(word_as_list)
                 if "_" not in word_completion:
                     guessed = True
-        elif len(guess) == len(word) and guess.isalpha():
+        elif len(guess) == len(slovo) and guess.isalpha():
             if guess in guessed_words:
                 print("Už si hádal tohle slovo", guess)
-            elif guess != word:
+            elif guess != slovo:
                 print(guess, "není to slovo.")
                 pokus -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
-                word_completion = word
+                word_completion = slovo
         else:
             print("Not a valid guess.")
         print(display_hangman(pokus))
@@ -54,11 +54,11 @@ def play(word):
     if guessed:
         print("Gratuluji, výhrál si!")
     else:
-        print("HMM... Slovo bylo " + word + ". Možná příště vyhraješ!")
+        print("HMM... Slovo bylo " + slovo + ". Možná příště vyhraješ!")
 
 
 def display_hangman(pokus):
-    stages = [  
+    obraz = [  
                 """
                    --------
                    |      |
@@ -129,15 +129,15 @@ def display_hangman(pokus):
                    -
                 """
     ]
-    return stages[pokus]
+    return obraz[pokus]
 
 
 def main():
-    word = get_word()
-    play(word)
+    slovo = get_word()
+    play(slovo)
     while input("Chceš hrát znova? (Y/N) ").upper() == "Y":
-        word = get_word()
-        play(word)
+        slovo = get_word()
+        play(slovo)
 
 
 if __name__ == "__main__":
